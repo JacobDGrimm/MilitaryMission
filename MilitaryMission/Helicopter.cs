17e67dea17857f275bf388ba63a02007f3fface4
Jacob Grimm <jacob.d.grimm@gmail.com>
2018-09-23 21:33:17 -0400

    adds parent and sub classes

---
+++ MilitaryMission/Helicopter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryMission
+{
+    class Helicopter : Vehicle
+    {
+        public void Fly()
+        {
+
+        }
+    }
+}
---
+++ MilitaryMission/Personell.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryMission
+{
+    class Personell
+    {
+        public void Shoot()
+        {
+
+        }
+        
+        public void Run()
+        {
+
+        }
+    }
+}
